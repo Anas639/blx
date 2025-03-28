8935a8a4b556a55c0d2c0a307d84bf09c94e349c
Anas Elmoutaouakil <anas.elmoutaouakil@gmail.com>
2025-03-28 00:33:11 +0000

    feat(tasks): add `--project` flag to `new` command to assign task to an existing project

--- cmd/task_new.go
+++ cmd/task_new.go
@@ -2,9 +2,12 @@ package cmd
 
 import (
 	"fmt"
+	"strconv"
 
+	"github.com/anas639/blx/internal/project"
 	"github.com/anas639/blx/internal/services"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 func newNewCmd(ctx *Context) *cobra.Command {
@@ -25,6 +28,16 @@ To start tracking time use the --start flag or run blx start <id>
 				return err
 			}
 
+			// check project id
+			projectFlag := cmd.Flag("project")
+			if projectFlag.Value.String() != "" {
+				project, err := assignTaskToProjectId(projectFlag, task.Id, taskService, services.NewProjectService(ctx.DB))
+				if err != nil {
+					fmt.Println(err.Error())
+				} else {
+					task.SetProject(project.Id, project.Name)
+				}
+			}
 			fmt.Println("Task successfully created ✅")
 			ctx.TaskPrinter.PrintSingle(task)
 			return nil
@@ -33,4 +46,20 @@ To start tracking time use the --start flag or run blx start <id>
 	newCmd.Flags().Int64P("project", "p", 0, "The ID of the project you want this task to be part of")
 	newCmd.Flags().BoolP("start", "s", false, "Marks this task as ongoing")
 	return newCmd
+}
+
+func assignTaskToProjectId(flag *pflag.Flag, taskId int64, taskService *services.TaskService, projectService *services.ProjectService) (*project.Project, error) {
+	projectId, err := strconv.Atoi(flag.Value.String())
+	if err != nil {
+		return nil, fmt.Errorf("Invalid project Id %s \n", flag.Value.String())
+	}
+	project, err := projectService.GetProjectById(int64(projectId))
+	if err != nil {
+		return nil, err
+	}
+	err = taskService.AssignProject(taskId, project.Id)
+	if err != nil {
+		return nil, err
+	}
+	return project, nil
 }
